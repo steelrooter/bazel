c0ac38c137f84f13ca6d9c7879696c294cd54caa
Sachin Tewari <xachin@gmail.com>
2020-05-14 20:33:28 +0530

    Use permanent dir for android_tools

--- create_tmp_android_tools.sh
+++ create_tmp_android_tools.sh
@@ -1,4 +1,7 @@
-rm -rf /tmp/android_tools
-mkdir /tmp/android_tools
-tar -xf bazel-bin/tools/android/runtime_deps/android_tools.tar.gz -C /tmp/android_tools
+ROOT_DIR="${HOME}/bin"
+TMP_ADR_DIR="/tmp/android_tools"
+FULL_ADR_DIR="${ROOT_DIR}${TMP_ADR_DIR}"
 
+rm -rf "${FULL_ADR_DIR}"
+mkdir -p "${FULL_ADR_DIR}"
+tar -xf bazel-bin/tools/android/runtime_deps/android_tools.tar.gz -C "${FULL_ADR_DIR}"
